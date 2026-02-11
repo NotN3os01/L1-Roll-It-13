9267005040cf0e36723687845efea4ad6b2915cd
Neos <choij4114@masseyhigh.school.nz>
2026-02-12 10:00:36 +1300

    Working single rounds

--- C_04_One_Round_v2.py
+++ C_04_One_Round_v2.py
@@ -18,6 +18,13 @@ def initial_points(which_player):
 
     return total, double
 
+
+def make_statement(statement, decoration):
+    """Adds emoji / additional characters to the start and end of headings"""
+
+    ends = decoration * 3
+    print(f"\n{ends} {statement} {ends}")
+
 # Main starts here...
 
 
@@ -78,4 +85,32 @@ while player_1_points < 13 and player_2_points <13:
 
     print(f"{first}: {player_1_points} | {second} {player_2_points}")
 
-print("end of round")
+# end of round
+
+# associate player points with either the user or the computer
+user_points = player_1_points
+comp_points = player_2_points
+
+# switch the user and computer points if the computer went first
+if first == "Computer":
+    user_points, comp_points = comp_points, user_points
+
+# work out who won...
+if user_points > comp_points:
+    winner = "User"
+else:
+    winner = "computer"
+
+round_feedback = f"The {winner} won."
+
+# double user points if eligible
+if winner == "User" and double_user == "yes":
+    user_points = user_points * 2
+
+# Output round results
+make_statement("Round Results", "=")
+print(f"User Points: {user_points} | Computer points: {comp_points}")
+print(round_feedback)
+print()
+
+
